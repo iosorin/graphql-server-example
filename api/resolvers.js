805d22da4c88e87a110b2dc4bed2bd7c204ab698
osorina <official.osorina@gmail.com>
2020-05-29 00:07:06 +0200

    finish mutation implementation, docs example

--- api/resolvers.js
+++ api/resolvers.js
@@ -1,6 +1,41 @@
-const todos = require('./todos');
+const todosSource = require('./todos');
 
-const getListByStatus = (status) => {
+let todos = JSON.parse(JSON.stringify(todosSource));
+
+const query = {
+    todo: ({ id }) => _find(id),
+    todos: ({ status }) => _getListByStatus(status),
+};
+
+const mutation = {
+    createTodo: ({ input }) => {
+        const todo = new _Todo(input);
+
+        todos.push(todo);
+
+        return todo;
+    },
+    updateTodo: ({ id, input }) => {
+        const todo = _find(id);
+
+        Object.assign(todo, input);
+
+        return todo;
+    },
+    deleteTodo: ({ id }) => {
+        todos = todos.filter(_byID(id, false));
+        console.log(100, todos);
+
+        return id;
+    },
+};
+
+const resolvers = {
+    ...query,
+    ...mutation
+};
+
+const _getListByStatus = (status) => {
     const completedList = () => todos.filter(todo => todo.completed);
     const uncompletedList = () => todos.filter(todo => !todo.completed);
 
@@ -16,7 +51,29 @@ const getListByStatus = (status) => {
     }
 };
 
-module.exports = {
-    todo: ({ id }) => todos.find(todo => todo.id == id),
-    todos: ({ status }) => getListByStatus(status)
+// UTILS
+const _byID = (id, equal = true) => {
+    return function(todo) {
+        return equal ? todo.id == id : todo.id != id;
+    };
 };
+
+const _find = (id) => {
+    return todos.find(_byID(id));
+};
+
+class _Todo {
+    static id() {
+        return Math.floor(Math.random() * (10000 - 1000) + 1000);
+    }
+
+    constructor({ title, completed = false, steps = [] }) {
+        this.id = _Todo.id();
+        this.title = title;
+        this.completed = completed;
+        this.steps = steps;
+    }
+}
+
+// EXPORT
+module.exports = resolvers;
