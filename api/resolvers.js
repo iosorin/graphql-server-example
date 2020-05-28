732bd820048e6cb5cb9262cd10670b8f9fbcb1d2
osorina <official.osorina@gmail.com>
2020-05-29 01:22:51 +0200

    finish graphql-server (connect mongoose, schema glb format)

--- api/resolvers.js
+++ api/resolvers.js
@@ -1,79 +1,26 @@
-const todosSource = require('./todos');
+const Todo = require('./model');
 
-let todos = JSON.parse(JSON.stringify(todosSource));
-
-const query = {
-    todo: ({ id }) => _find(id),
-    todos: ({ status }) => _getListByStatus(status),
+const queries = {
+    todo: ({ id }) => Todo.findById(id),
+    todos: ({ status }) => Todo.find({ status }),
 };
 
-const mutation = {
+const mutations = {
     createTodo: ({ input }) => {
-        const todo = new _Todo(input);
-
-        todos.push(todo);
-
-        return todo;
+        return Todo.create(input);
     },
     updateTodo: ({ id, input }) => {
-        const todo = _find(id);
-
-        Object.assign(todo, input);
-
-        return todo;
+        return Todo.findByIdAndDelete(id, input, { new: true });
     },
     deleteTodo: ({ id }) => {
-        todos = todos.filter(_byID(id, false));
-        console.log(100, todos);
-
-        return id;
+        return Todo.deleteOne({ _id: id }).then(() => id);
     },
 };
 
 const resolvers = {
-    ...query,
-    ...mutation
+    ...queries,
+    ...mutations
 };
-
-const _getListByStatus = (status) => {
-    const completedList = () => todos.filter(todo => todo.completed);
-    const uncompletedList = () => todos.filter(todo => !todo.completed);
-
-    switch (status) {
-        case 'COMPLETED':
-            return completedList();
-
-        case 'UNCOMPLETED':
-            return uncompletedList();
-
-        default:
-            return todos;
-    }
-};
-
-// UTILS
-const _byID = (id, equal = true) => {
-    return function(todo) {
-        return equal ? todo.id == id : todo.id != id;
-    };
-};
-
-const _find = (id) => {
-    return todos.find(_byID(id));
-};
-
-class _Todo {
-    static id() {
-        return Math.floor(Math.random() * (10000 - 1000) + 1000);
-    }
-
-    constructor({ title, completed = false, steps = [] }) {
-        this.id = _Todo.id();
-        this.title = title;
-        this.completed = completed;
-        this.steps = steps;
-    }
-}
 
 // EXPORT
 module.exports = resolvers;
